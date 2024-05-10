4f75273cc3532d44251b776c4c3b73e4552db46b
Datnguyen273 <167218606+Datnguyen273@users.noreply.github.com>
2024-05-10 14:50:23 +0700

    update term+policy

---
+++ term.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>pepicase</title>
+        <link rel="stylesheet" href="/pepicase/header-footer.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lexend+Tera:wght@100..900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/pepicase/fonts.css">
+    </head>
+    <body>
+        <div class = "lexend-deca sales d-flex justify-content-center align-items-center" style ="height: 50px; font-size: 15px;">
+            <div align = "center"><text>Free Shipping worldwide for<br>orders $30 or above</text></div>
+            <div style="padding-left:165px; padding-right:165px;" align = "center"><text>ALL PHONE CASES: Limited-time<br>40% Off</text></div>
+            <div align = "center"><text>30 days free replacements<br>for quality issues.</text></div>
+            
+        </div>
+        <header>
+            <div class="header">
+                <div class="top_header">
+                    <nav class="d-flex justify-content-between">
+                        <a href="" id="search">
+                            <img src="/pepicase/pics/Search.png" alt="">
+                        </a>
+                        <h2 class="name"><a href="/pepicase/index.php" style = "color:inherit; text-decoration:none;">pepicase</a></h2>
+                        <div class="logo_right">
+                            <a href="" style="margin-right:20px; text-decoration: none; color:white;">
+                                <img src="/pepicase/pics/Vector (1).png" alt="">
+                            </a>
+                            <a href="" style="margin-right: 20px; text-decoration: none; color:white;">
+                                <img src="/pepicase/pics/Cart.png" alt="">
+                            </a>
+                            <a href="" style="text-decoration: none; color:white;">
+                                <img src="/pepicase/pics/Frame.png" alt="">
+                            </a>
+                        </div>
+                    </nav>
+                </div>
+                <div class="bottom_header">
+                    <nav class="container">
+                        <ul id="main_menu">
+                            <li><a href="">New & Featured</a></li>
+
+                            <li>
+                                <a href="">Products</a>
+                                <span class="icon" style="color: blue; cursor: pointer;">></span>
+                                <ul class="sub_menu">
+                                    <li><a href="">Menu 2.1</a></li>
+                                    <li><a href="">Menu 2.2</a></li>
+                                    <li><a href="">Menu 2.3</a></li>
+                                    <li><a href="">Menu 2.4</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="">Collections</a>
+                                <span class="icon" style="color: blue; cursor: pointer;">></span>
+                                <ul class="sub_menu">
+                                    <li><a href="">Menu 3.1</a></li>
+                                    <li><a href="">Menu 3.2</a></li>
+                                    <li><a href="">Menu 3.3</a></li>
+                                    <li><a href="">Menu 3.4</a></li>
+                                </ul>
+                            </li>
+
+                            <li><a href="">About Us</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+
+        <div class="lexend d-flex justify-content-center align-items-center" style="font-size: 35px;font-weight:bold">PEPICASE: TERMS OF USE</div>
+        <div class="d-flex justify-content-center" style = "width:100vw;">
+            <hr style="width: 1133px; height:3px;">
+        </div>
+        <div class ="lexend-light d-flex justify-content-center">
+            <p style = "width: 1133px; height: fit-content; font-size:22px;">
+            <strong>Introduction</strong><br>
+            These Terms of Use (“Terms”) are a legal agreement entered into by and between you and Pepicase, Inc. (“Company,” “we,” “our,” or “us”) and govern your access to and use of the “Site” and any content, documentation, software, text, images, video, audio, and services offered on or made available through the Site (collectively, the “Services”), whether as a visitor or registered user. Your access to and use of the Services is conditioned on your acceptance of these Terms and any additional terms that may be provided or presented to you when you use certain features of the Services.
+            Please read these Terms carefully. By registering for an account, making a purchase through the Services, or otherwise accessing or using the Services, you agree to be bound by these Terms. If you do not agree to be bound by these Terms, do not access or use the Services, create an account, make a purchase through the Services, or otherwise indicate that you agree to these Terms.
+            These Terms contain a mandatory Arbitration provision below that requires the use of arbitration on an individual basis to resolve disputes or claims related to these Terms or your use of the Services, rather than jury trial or class actions. Please see the mandatory arbitration section below.
+            <br><br>
+            <strong>Changes to the Terms of Use</strong><br>
+            We may revise and update these Terms of Use from time to time in our sole discretion. All changes are effective immediately when we post them, and apply to all access to and use of the Services thereafter. However, any changes to dispute resolution provisions set forth below will not apply to any disputes for which the parties have actual notice prior to the date the change is posted within our Services.
+            <br><br>
+            <strong>Changes to the Services</strong><br>
+            We may update the content on the Services from time to time, but related content is not necessarily complete or up-to-date. Any of the material on the Services may be out of date at any given time, and we are under no obligation to update such material.
+            <br><br>
+            <strong>Age Requirements</strong><br>
+            By using our Services, you represent and warrant that you are: (a) over 18 years of age or the age of majority in your jurisdiction, whichever is greater; (b) of legal age to form a binding contract; and (c) not a person barred from using the Services under the laws of your country of residence or any other applicable jurisdiction.
+            <br><br>
+            <strong>Privacy Notice</strong><br>
+            In addition to these Terms, the Casely Privacy Notice governs all information you provide to us via the Services, and you acknowledge the privacy practices we cover on our Privacy Notice.
+            <br><br>
+            <strong>Accessing the Services</strong><br>
+            You may browse the Services without registering, but some features or functionality may not be accessible unless you register. In registering for the Services, you agree to: (i) provide true, accurate, current, and complete information about yourself as prompted by the Services’ registration form; and (ii) maintain and promptly update the information to keep it true, accurate, current, and complete. If you provide any information that is untrue, inaccurate, not current, or incomplete, or we reasonably suspect that you have done so, we may suspend or terminate your account.
+            You are fully responsible for all activities that occur under your account. You may not share your account or password with anyone. You agree to notify us immediately of any unauthorized use of your account or password or any other similar breach of security. You also agree to ensure that you exit from your account at the end of each session. You should use particular caution when accessing your account from a public or shared computer so that others are not able to view or record your password or other personal information.
+            <br><br>    
+            <strong>Method of Payment</strong><br>
+            All payments are independently processed through a third-party service, so Casely does not collect any payment information, such as credit card details. If payments through the available methods on the Services fail or are refused by the payment service provider, Casely is under no obligation to fulfill the purchase order. Any possible costs or fees resulting from the failed or refused payment will be borne by you.       
+            <br><br>
+            <strong>Content</strong><br>
+            All information, data, text, software, music, sounds, images, graphics, videos, messages, scripts, tags, and other materials accessible through the Services, whether publicly posted or privately transmitted (“Content”), are the sole responsibility of the person from whom such Content originated. This means that you, and not Pepicase, are entirely responsible for all Content that you upload, post, transmit, or otherwise make available through the Services (“Your Content”), and other users of the Services, and not Pepicase, are similarly responsible for all Content they upload, post, transmit, or otherwise make available through the Services (“User Content”).
+            Pepicase has no obligation to pre-screen Content (which includes without limitation Your Content and User Content), although we reserve the right in our sole discretion to pre-screen, refuse, or remove any Content. Without limiting the generality of the foregoing sentence, we have the right to remove any Content that violates these Terms. We do not warrant the accuracy, completeness, or usefulness of Content. Any reliance you place on such information is strictly at your own risk.
+            To the extent that you submit any Content, you represent and warrant that: (i) you have all necessary right and authority to grant the rights set forth in these Terms with respect to Your Content; and (ii) Your Content does not violate or misappropriate any intellectual property or other right of any other party.
+            <br><br>
+            <strong>Feedback</strong><br>
+            If you elect to provide or make available to Casely any suggestions, comments, ideas, improvements, or other feedback relating to the Services (“Feedback”), you grant us the right to use, reproduce, modify, adapt, create derivative works from, publicly perform, publicly display, distribute, make, have made, assign, pledge, transfer, or otherwise grant rights in your Feedback in any form and any medium (whether now known or later developed), without credit or compensation to you.
+            Prohibited Uses
+            <br><br>
+            <strong>Intellectuall Property Rights</strong><br>
+            Unless subject to a separate written agreement with us, Pepicase does not claim ownership of Your Content. However, you grant us and our service providers a worldwide, royalty-free, fully-paid-up, non-exclusive, sublicensable, transferable license to use, reproduce, modify, adapt, create derivative works from, publicly perform, publicly display, distribute, make and have made your Content (in any form and any medium, whether now known or later developed) in connection with the Services.
+            Except with respect to Your Content, Pepicase and our licensors own all right, title, and interest in the Services and all Content and other materials within the Service. The Pepicase logos and trademarks referenced in the Services are the trademarks of Pepicase and our affiliates. Any other company names, product names, service names, and logos referenced in the Services may be the trademarks of their respective owners. Pepicase reserves all rights not expressly granted to you.
+            To the extent the Services provide access to any online software or other similar components, then Pepicase grants you a limited, non-exclusive, non-transferable, non-sublicensable, revocable license to access and use the software or components only in connection with the Services.
+            <br><br>
+            <strong>Limitation on Liability</strong><br>
+            TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW, PEPICASE PARTIES WILL NOT BE LIABLE FOR ANY LOST PROFITS OR INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE, OR CONSEQUENTIAL DAMAGES, EVEN IF PEPICASE PARTIES HAD ACTUAL OR CONSTRUCTIVE KNOWLEDGE OF THE POSSIBILITY OF SUCH DAMAGES AND REGARDLESS OF WHETHER SUCH DAMAGES WERE FORESEEABLE. TO THE FULLEST EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT WILL PEPICASE PARTIES’ TOTAL LIABILITY TO YOU FOR ALL CLAIMS ARISING FROM OR RELATING TO THESE TERMS OR YOUR ACCESS TO OR USE OF (OR INABILITY TO ACCESS OR USE) THE SERVICES EXCEED THE GREATER OF $25 OR THE AMOUNT PAID BY YOU TO PEPICASE FOR ACCESS TO THE SERVICES (OR PORTION THEREOF) AT ISSUE WITHIN THE 6 MONTHS IMMEDIATELY PRECEDING THE DATE ON WHICH THE APPLICABLE CLAIM AROSE. THIS LIMITATION OF LIABILITY DOES NOT APPLY TO LIABILITY RESULTING FROM PEPICASE PARTIES’ GROSS NEGLIGENCE, FRAUD, OR INTENTIONAL MISCONDUCT.
+            <br><br>
+            <strong>Copyright</strong><br>
+            If you believe that your work or content has been made available through the Services in a way that constitutes copyright infringement, please provide Pepicase’s Agent for Notice of Copyright the following information: (i) a physical or electronic signature of a person authorized to act on behalf of the owner of an exclusive right that is allegedly infringed; (ii) identification of the copyrighted work claimed to have been infringed, or, if multiple works are on a single site, a representative list of such works; (iii) identification of the infringing material or activity (or the reference or link to such material) and information reasonably sufficient to permit us to locate the material (or the reference or link); (iv) contact information for the copyright owner or authorized agent; (v) a statement by you that you have a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law; and (vi) a statement by you that the information in the notice is accurate, and under penalty of perjury, that you are authorized to act on behalf of the copyright owner.
+            <br><br>
+            <strong>Miscellaneous</strong><br>
+            These Terms constitutes the entire agreement between you and Pepicase concerning your access to and use of the Services. It supersedes all prior or contemporaneous oral or written negotiations and agreements between you and Pepicase with respect to its subject matter. You may not assign, transfer, or delegate any right or obligation under these Terms without our prior written consent. Any attempted assignment, transfer, or delegation by you in violation of this Section is null and void in each instance. We may assign or transfer these Terms to any affiliate or in connection with a merger, acquisition, or sale of substantially all of our assets without your consent. The failure of Pepicase to exercise or enforce any right or provision of these Terms will not constitute a waiver of such right or provision. If any provision of these Terms is held to be invalid or unenforceable under applicable law, then such provision will be construed, limited, modified, or, if necessary, severed to the extent necessary to eliminate its invalidity or unenforceability, without in any way affecting the remaining parts of these Terms. Any prevention of or delay in performance by Pepicase due to labor disputes, acts of god, governmental restrictions, enemy or hostile governmental action, fire or other casualty, or other causes beyond its reasonable control will excuse the performance of its obligations for a period equal to the duration of any such prevention or delay.
+        </p>
+        </div>
+        <br>
+    <footer class="footer">
+        <div class = "main-content">
+            <div class = "rows">
+                <div class = "column">
+                    <p class="title">Policy</p>
+                    <div class="footer-links">
+                        <a href="">Trading conditions</a><br>
+                        <a href="">Protect user information</a><br>
+                        <a href="">Secure customer transactions</a><br>
+                        <a href="">Product warranty policy</a><br>
+                        <a href="">Payment policies and regulations</a>
+                    </div>
+                </div>
+                <div class = "column">
+                    <p class="title">Customer Support</p>
+                    <div class="footer-links">
+                        <a href="">Shopping guide</a><br>
+                        <a href="">Electronic bill</a><br>
+                        <a href="">Shipping and delivery</a><br>
+                        <a href="">Payment methods</a><br>
+                        <a href="">Look up orders</a>
+                    </div> 
+                </div>
+                <div class = "column">
+                    <p class="title">Address</p>
+                    <div class="footer-a">
+                        <a href=""><img src="/pepicase/pics/location.svg" alt="location-icon"></a><br>
+                        <a href=""><img src="/pepicase/pics/sms.svg" alt="sms-icon"></a><br>
+                        <a href=""><img src="/pepicase/pics/call.svg" alt="call-icon"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+            <div class ="icons d-flex justify-content-center align-items-center" style = "height: 250px; padding-bottom: 30px;">
+                <a href=""><img src="/pepicase/pics/Facebook Icon.png" class = "img-rounded" alt="facebook-icon" style ="margin-right: 40px;"></a>
+                <a href=""><img src="/pepicase/pics/Group 1.png" class = "img-rounded" alt="facebook-icon"></a>
+                <a href=""><img src="/pepicase/pics/x.png" class = "img-rounded" alt="facebook-icon" style ="margin-left: 40px;"></a>
+            </div>
+
+            <div class = "bottom-nav lexend" style = "margin-left: 45px; padding-bottom: 20px; font-size: 20px;">
+                <a href="">TERMS OF USE</a>
+                <a href="">PRIVACY POLICY</a>
+                <a href="">PR/COLLAB</a>
+                <a href="">COPYRIGHT INFO</a>
+                <a href="">FAQs</a>
+                <a href="">HELP</a>
+            </div>
+
+        </footer>
+        <script src = "general.js"></script>
+    </body>
+</html>
