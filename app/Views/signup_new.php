2d3c868a4284e1490a3ff6e598f5db0c856c4cff
xuanquynh298 <167272229+xuanquynh298@users.noreply.github.com>
2024-05-17 14:45:18 +0700

    update check password + confirm password

--- app/Views/signup_new.php
+++ app/Views/signup_new.php
@@ -3,7 +3,7 @@
 <?php include(APPPATH.'views/components/top-header.php'); ?>
 
         <div class="d-flex align-items-center justify-content-center">
-            <form method="post" action="/pepicase/public/signup/">
+            <form method="post" action="/pepicase/public/signup/" onsubmit="return checkpassword()">
                 <h2 class="text-center" style="margin: 0px; margin-bottom: 20px; color: #1F3E97; font-family: 'Londrina Solid';"><b>SIGN UP</b></h2>
 
                 <div class="mb-3">
@@ -19,14 +19,14 @@
                     <label for="password" class="form-label"><b>Password</b></label>
                     
                     <div class="input-group">
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Abc12345" style="border-right: none;" oninput="check()">
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Abc12345" style="border-right: none;" oninput="check(); check_confirm_password()">
                         <span style="background: white; cursor: pointer; border-left: none;" class="input-group-text">
                             <img style="margin: 0px;" id="toggle_password" src="/pepicase/public/pics/Ellipse.svg" alt="">
                         </span>
                     </div>
 
                     <div id="check_password">
-                        <span style="color: red;">Mật khẩu phải bao gồm chữ in hoa, chữ viết thường và số</span>
+                        <span style="color: #1F3E97; font-family: 'Lexend'">Mật khẩu phải tối thiểu 8 ký tự và bao gồm chữ in hoa, chữ viết thường và số</span>
                     </div>
                 </div>
 
@@ -41,15 +41,15 @@
                     </div>
 
                     <div id="check_confirm_password">
-                        <span></span>
+                        <span style="color: #1F3E97; font-family: 'Lexend'"></span>
                     </div>
                 </div>
 
                 <div class="d-grid gap-2">
-                    <a href="#" class="btn btn-primary" 
-                    style = "width: 100%; margin-top: 20px; background-color: #FFE57A; border-color: black; transition: background-color 0.3s; color:black; font-family: 'Lexend'"
-                    onclick="checkpassword()">SIGN UP</a>
+                    <input class="btn btn-primary" type="submit" name="signup" value="SIGN UP" onclick="checkpassword()" 
+                    style = "width: 100%; margin-top: 20px; background-color: #FFE57A; border-color: black; transition: background-color 0.3s; color:black; font-family: 'Lexend'">
                 </div>
+                <div id="trigger_confirm_password" class="message"></div>
 
                 <div class="d-flex justify-content-center" style="margin-top: 30px; margin-bottom: 30px">
                     <hr class="before" style="width: 100%">
@@ -78,7 +78,7 @@
             </form>
         </div>
 
-<?php include(APPPATH.'views/components/bottom-footer.php'); ?>
+        <?php include(APPPATH.'views/components/bottom-footer.php'); ?>
 
         <script>
             // nhấn vào con mắt sẽ hiện ra password
@@ -99,22 +99,6 @@
                 }
             });
 
-            // ràng buộc email đúng định dạng
-            function check_email() {
-                var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                return emailRegex.test(email);
-
-                var emailAddress = "example@example.com"; // Thay đổi địa chỉ email tại đây
-                if (isValidEmail(emailAddress)) {
-                    console.log("Địa chỉ email hợp lệ.");
-                } else {
-                    console.log("Địa chỉ email không hợp lệ.");
-                }
-
-            }
-
-
-            // ràng buộc password có chữ in hoa, chữ thường, số
             function check() {
                 var password = document.getElementById("password").value;
                 var confirm_password = document.getElementById("confirm_password").value;
@@ -122,22 +106,58 @@
                 var trigger_confirm_password = document.getElementById("check_confirm_password");
 
                 // Kiểm tra mật khẩu
-if ((password.length >= 8) && (password.match(/[0-9]/)) && (password.match(/[A-Z]/)) && (password.match(/[a-z]/))) {
+                if ((password.length >= 8) && (password.match(/[0-9]/)) && (password.match(/[A-Z]/)) && (password.match(/[a-z]/))) {
                     trigger_password.style.color = "green";
+                    trigger_password.style.fontFamily = "Lexend";
                     trigger_password.innerText = "Mật khẩu hợp lệ.";
                 } else {
                     trigger_password.style.color = "red";
+                    trigger_password.style.fontFamily = "Lexend";
                     trigger_password.innerText = "Mật khẩu không chính xác. Vui lòng nhập lại!";
                 }
+            }
 
-                /* Kiểm tra xác nhận mật khẩu
+            function check_confirm_password() {
+                var password = document.getElementById("password").value;
+                var confirm_password = document.getElementById("confirm_password").value;
+                var trigger_confirm_password = document.getElementById("check_confirm_password").getElementsByTagName('span')[0];
+
+                // Kiểm tra xác nhận mật khẩu
                 if (confirm_password === password) {
                     trigger_confirm_password.style.color = "green";
+                    trigger_confirm_password.style.fontFamily = "Lexend";
                     trigger_confirm_password.innerText = "Xác nhận mật khẩu hợp lệ.";
                 } else {
                     trigger_confirm_password.style.color = "red";
+                    trigger_confirm_password.style.fontFamily = "Lexend";
                     trigger_confirm_password.innerText = "Xác nhận mật khẩu không khớp. Vui lòng nhập lại!";
-                } */
+                }
+            }
+
+            // kiểm tra password và confirm password trùng khớp chưa khi nhấn submit
+            function checkpassword() {
+                var password = document.getElementById("password").value;
+                var confirm_password = document.getElementById("confirm_password").value;
+                var trigger_confirm_password = document.getElementById("trigger_confirm_password");
+
+                if ((password.length >= 8) && (password.match(/[0-9]/)) && (password.match(/[A-Z]/)) && (password.match(/[a-z]/))) {
+                    if (confirm_password === password) {
+                        trigger_confirm_password.style.color = "green";
+                        trigger_confirm_password.style.fontFamily = "Lexend";
+                        trigger_confirm_password.innerText = "Xác nhận mật khẩu hợp lệ.";
+                        return true; // Submit form
+                    } else {
+                        trigger_confirm_password.style.color = "red";
+                        trigger_confirm_password.style.fontFamily = "Lexend";
+                        trigger_confirm_password.innerText = "Xác nhận mật khẩu không khớp. Vui lòng nhập lại!";
+                        return false; // Prevent form submission
+                    }
+                } else {
+                    trigger_confirm_password.style.color = "red";
+                    trigger_confirm_password.style.fontFamily = "Lexend";
+                    trigger_confirm_password.innerText = "Mật khẩu không chính xác. Vui lòng nhập lại!";
+                    return false; // Prevent form submission
+                }
             }
         </script>
     </body>
