202218063167bd9b7de760da13c33520a91d7330
tk <milkcucheo@gmail.com>
2024-05-17 13:55:29 +0700

    ~

---
+++ app/Views/wishlist.php
@@ -0,0 +1,7 @@
+<?php include(APPPATH.'views/components/usual-links.php'); ?>
+  <link rel="stylesheet" href="/pepicase/public/css/wishlist.css">
+<?php include(APPPATH.'views/components/top-header.php'); ?>
+
+    
+    <div class="fave-item" tabindex="1" draggable="true" id="
+<?php include(APPPATH.'views/components/bottom-footer.php'); ?>
