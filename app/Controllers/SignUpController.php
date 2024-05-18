203a769fb6f228db02936bc5ce85d09ff70bd953
JohnMaxes <baodang9855.tdn@gmail.com>
2024-05-18 22:49:34 +0700

    signup php k có lỗi nhưng mất định dạng

--- app/Controllers/SignUpController.php
+++ app/Controllers/SignUpController.php
@@ -33,83 +33,16 @@ class SignUpController extends BaseController
         return $decrypted;
     }
 
-    public function send_signup_mail()
+    public function send_signup_email()
     {
-        $rules = [
-            'email' => 'valid_email',
-        ];
-
-        $data = [
-            'validation' => '',
-        ];
-
-        $email = $this->request->getPost('email');
+        $mail = $this->request->getPost('email');
         $password = $this->request->getPost('password');
-        $data = array (
-            'email' => $email,
-            'password' => $password,
-        );
-
-        if($this->validate($rules))
-        {
-            $user = new User($email, $password);
-            {
-                if ($user->check_if_authorized()) 
-                {
-                    $message = [
-                        "message"=> "User already exist!",
-                    ];
-                    return view('signup_fail', $message);
-                }
-                else
-                {
-                    $encryptedEmail = $this->encrypt($email);
-                    $encryptedPassword = $this->encrypt($password);
-                    $token = rand(0000,9999);
-                    $new_url = 'localhost/pepicase/public/signup/' . $encryptedEmail .'/'. $encryptedPassword;
-                    
-
-                    //khởi tạo và gửi mail tại đây Quỳnh!
-                    $title = "[Pepicase] - Xác nhận đăng ký tài khoản";
-                    $content1 = "Xin chào ... ,";
-                    $content2 = "Bạn vui lòng kích hoạt tài khoản tại đây giúp chúng mình nhé: ".FCPATH.'?token='.$token.'&email'.$email;
-                    $content3 = "Pepicase xin cảm ơn bạn!";
-                    $to_email = $email;
-                    $this->send_signup_mail($to_email, $content1, $content2, $content3);
-                }
-            };
-        }
-        else
-        {
-            $message = [
-                "message"=> "Please enter a valid email!",
-            ];
-            return view('signup_fail', $message);
-        }
-        
-    }
-
-    public function signup($encryptedEmail, $encryptedPassword)
-    {
-        $decryptedEmail = $this->decrypt($encryptedEmail);
-        $decryptedPassword = $this->decrypt($encryptedPassword);
-        $user = new User($decryptedEmail, $decryptedPassword);
-        if(!$user->check_if_authorized())
-        {
-            $user->create($decryptedEmail, $decryptedPassword); //chưa code hàm create!
-            //tìm hiểu làm sao để biết create thành công hay không, sau đó check, nếu thành công thì về '/' nếu không thì về signup_fail
-            //tìm hiểu làm sao để biết create thành công hay không, sau đó check, nếu thành công thì về '/' nếu không thì về signup_fail
-            //tìm hiểu làm sao để biết create thành công hay không, sau đó check, nếu thành công thì về '/' nếu không thì về signup_fail
-            redirect()->to('/');
-        }
-        else 
-        {
-            return redirect()->to ('/');
-        }
+        $confirmedPassword = $this->request->getPost('confirm_password');
+        return redirect() -> to ('/collections');
     }
 
     public function index(): string
     {
-        return view ('signup_new');
+        return view ('signup');
     }
 }
