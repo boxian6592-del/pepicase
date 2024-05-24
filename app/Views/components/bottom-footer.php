cd0821b6f37bc14dc861272ba44ed93487df2a90
PhamHa-2004 <22520374@gm.uit.edu.vn>
2024-05-24 17:12:52 +0700

    Sửa footer

--- app/Views/components/bottom-footer.php
+++ app/Views/components/bottom-footer.php
@@ -1,23 +1,24 @@
 <footer class="footer">
         <div class = "main-content">
-            <p class="title">Address</p>
-            <div class="footer-a">
-                <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon"></a><br>
-                <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon"></a><br>
+            <div class="footer-a" style="font-size: 20px; margin-left: 60px;"><br>
+                <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon"></a>
+                <a style="margin-left: 5px;">Quarter 6, Linh Trung Ward, Thu Duc City, Ho Chi Minh City.</a><br>
+                <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon"></a>
+                <a style="margin-left: 5px"> pepicase@gmail.com</a><br>
                 <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon"></a>
+                <a style="margin-left: 5px"> (028) 99999999</a>
             </div>
         </div>
 
-            <div class ="icons d-flex justify-content-center align-items-center" style = "height: 250px; padding-bottom: 30px;">
-                <a href=""><img src="/pepicase/public/pics/Facebook Icon.png" class = "img-rounded" alt="facebook-icon" style ="margin-right: 40px;"></a>
+            <div class ="icons d-flex justify-content-center align-items-center" style = "height: 50px; padding-bottom: 60px; margin-left: 650px">
+                <a href=""><img src="/pepicase/public/pics/Facebook Icon.png" class = "img-rounded" alt="facebook-icon" style ="margin-right: 60px;"></a>
                 <a href=""><img src="/pepicase/public/pics/Group 1.png" class = "img-rounded" alt="facebook-icon"></a>
-                <a href=""><img src="/pepicase/public/pics/x.png" class = "img-rounded" alt="facebook-icon" style ="margin-left: 40px;"></a>
+                <a href=""><img src="/pepicase/public/pics/x.png" class = "img-rounded" alt="facebook-icon" style ="margin-left: 60px;"></a>
             </div>
 
-            <div class = "bottom-nav lexend" style = "margin-left: 45px; padding-bottom: 20px; font-size: 20px;">
+            <div class = "bottom-nav lexend" style = "margin-left: 90px; padding-bottom: 20px; font-size: 20px;">
                 <a href="">TERMS OF USE</a>
                 <a href="">PRIVACY POLICY</a>
-                <a href="">PR/COLLAB</a>
                 <a href="">COPYRIGHT INFO</a>
                 <a href="">FAQs</a>
                 <a href="">HELP</a>
