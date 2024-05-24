85e73c616e583b45871c5abe976d229e0c51307e
PhamHa-2004 <22520374@gm.uit.edu.vn>
2024-05-24 17:15:11 +0700

    update empty cart

---
+++ app/Views/emptycart.php
@@ -0,0 +1,19 @@
+<?php include(APPPATH.'views/components/usual-links.php'); ?>
+<?php include(APPPATH.'views/components/top-header.php'); ?>
+  
+  
+<div class ="container-fluid d-flex flex-column align-items-center" style = "margin: 0; padding: 0; min-height: 60vh">
+      <div class = "lexend-tera container-fluid d-flex flex-column justify-content-center align-items-center" style = "height:200px; background-color:#FFFAE3; font-weight: 600;">
+         <div class = "lexend-tera container-fluid d-flex flex-column justify-content-center align-items-center" style = "font-size: 40px; font-weight: 600">Your Cart </div><br>
+         <div class="public sans container-fluid d-flex justify-content-center align-items-center" style="font-size:15px; font-weight: 200" >
+         <p > Not ready to checkout?<p>
+         <a href="/pepicase/public/"> Continue Shopping</a>        
+          </div>
+      </div>
+
+      <div class="container-fluid lexend-tera d-flex align-items-center " style = "font-size: 45px; margin-top:80px; font-weight: 600; margin-left: 40px">Your Cart</div>
+      <div class="container-fluid lexend d-flex align-items-center " style = "font-size: 20px; margin-top:20px; font-weight: 200; margin-left: 50px">Your cart is currently empty.</div>
+ 
+</div>
+
+ <?php include(APPPATH.'views/components/bottom-footer.php'); ?>
