34bdd0ea1659b2fa7874e1969a7a5882af408881
JohnMaxes <baodang9855.tdn@gmail.com>
2024-05-27 07:45:52 +0700

    update new footer

--- app/Views/components/bottom-footer.php
+++ app/Views/components/bottom-footer.php
@@ -1,27 +1,32 @@
 
 <footer class="footer" style="line-height: 60px">
-    <div class="container">
-        <div class = "d-flex flex-column justify-content-center align-items-center">
-            <div class="footer-a lexend" style="font-size: 20px;line-height: 35px; margin-top: 10px">
+
+        <div class = "d-flex flex-row justify-content-center align-items-center">
+
+            <div class="footer-a lexend-deca" style="font-size: 20px;line-height: 35px; width: 40vw;">
                 <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon"></a>
-                <a style="margin-left: 5px;">Quarter 6, Linh Trung Ward, Thu Duc City, Ho Chi Minh City.</a><br>
+                <a style="margin-left: 5px;">Quarter 6, Linh Trung Ward, Thu Duc, Ho Chi Minh City.</a><br>
                 <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon"></a>
                 <a style="margin-left: 5px"> pepicase@gmail.com</a><br>
                 <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon"></a>
                 <a style="margin-left: 5px"> (028) 99999999</a>
             </div>
-        </div>
 
-            <div class ="icons d-flex justify-content-center align-items-center" style = "height: 100px; margin-top: 40px; margin-left: 145px;">
-                <a href=""><img src="/pepicase/public/pics/Facebook Icon.png" class = "img-rounded" alt="facebook-icon" style ="margin-right: 60px; height: 100%; width: auto;"></a>
-                <a href=""><img src="/pepicase/public/pics/Group 1.png" class = "img-rounded" alt="facebook-icon" style = "height: 100%; width: auto;"></a>
-                <a href=""><img src="/pepicase/public/pics/x.png" class = "img-rounded" alt="facebook-icon" style ="margin-left: 60px; "></a>
+            <div class ="d-flex justify-content-center align-items-center" style = "height: 10vh;">
+                <a href=""><img src="/pepicase/public/pics/Facebook Icon.svg" class = "img-rounded" alt="facebook-icon" style ="margin-right: 60px; height: 7vh; width: auto;"></a>
+                <a href=""><img src="/pepicase/public/pics/Group 1.svg" class = "img-rounded" alt="facebook-icon" style = "height: 7vh; width: auto;"></a>
+                <a href=""><img src="/pepicase/public/pics/x.svg" class = "img-rounded" alt="facebook-icon" style ="margin-left: 60px; height: 7vh; width: auto;"></a>
+            </div>
+
+            <div class = "d-flex justify-content-center align-items-center" style = "height: 10vh; width: 20vw;">
+                <a style="text-decoration: none; font-weight: bold; font-size: 45px; color: black; font-family: 'Londrina Solid';" href="/pepicase/public/">pepicase</a>
             </div>
 
         </div>
-            <div class = "bottom-nav lexend" style = "margin-left: 90px; padding-bottom: 20px; font-size: 20px;">
-                <a href="">TERMS OF USE</a>
-                <a href="">PRIVACY POLICY</a>
+
+            <div class = "bottom-nav lexend" style = "margin-left: 10vw; padding-bottom: 20px; font-size: 20px;">
+                <a href="/pepicase/public/terms-of-use">TERMS OF USE</a>
+                <a href="/pepicase/public/policy">PRIVACY POLICY</a>
                 <a href="">COPYRIGHT INFO</a>
                 <a href="">FAQs</a>
                 <a href="">HELP</a>
@@ -29,3 +34,5 @@
         </footer>
     </body>
 </html>
+
+
