50fa66404168791b8af4b9b0838565838ebdb32e
tk <milkcucheo@gmail.com>
2024-06-04 02:37:49 +0700

    redesign footer

--- app/Views/components/bottom-footer.php
+++ app/Views/components/bottom-footer.php
@@ -3,34 +3,20 @@
 
         <div class = "d-flex flex-row justify-content-center align-items-center">
 
-            <div class="footer-a lexend-deca" style="font-size: 20px;line-height: 35px; width: 40vw;">
-                <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon"></a>
-                <a style="margin-left: 5px;">Quarter 6, Linh Trung Ward, Thu Duc, Ho Chi Minh City.</a><br>
-                <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon"></a>
-                <a style="margin-left: 5px"> pepicase@gmail.com</a><br>
-                <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon"></a>
-                <a style="margin-left: 5px"> (028) 99999999</a>
-            </div>
-
-            <div class ="d-flex justify-content-center align-items-center" style = "height: 10vh;">
-                <a href=""><img src="/pepicase/public/pics/Facebook Icon.svg" class = "img-rounded" alt="facebook-icon" style ="margin-right: 60px; height: 7vh; width: auto;"></a>
-                <a href=""><img src="/pepicase/public/pics/Group 1.svg" class = "img-rounded" alt="facebook-icon" style = "height: 7vh; width: auto;"></a>
-                <a href=""><img src="/pepicase/public/pics/x.svg" class = "img-rounded" alt="facebook-icon" style ="margin-left: 60px; height: 7vh; width: auto;"></a>
-            </div>
-
-            <div class = "d-flex justify-content-center align-items-center" style = "height: 10vh; width: 20vw;">
-                <a style="text-decoration: none; font-weight: bold; font-size: 45px; color: black; font-family: 'Londrina Solid';" href="/pepicase/public/">pepicase</a>
-            </div>
-
+        <div class="footer-a lexend" style="font-size: 20px;line-height: 40px; width: 75vw;">
+          <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon" style="margin-left: 171px;"></a>
+          <a style="margin-left: 5px;">Thu Duc City, Ho Chi Minh City</a>
+          <a href="/pepicase/public/terms-of-use" style="margin-left: 350px; font-size: 20px;" id="terms-of-use">TERMS OF USE</a><br>
+          <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon" style="margin-left: 171px;"></a>
+          <a style="margin-left: 5px"> pepicasesupport@gmail.com</a>
+          <a href="/pepicase/public/policy" style="margin-left: 360px; font-size: 20px;" id="terms-of-use">PRIVACY POLICY</a><br>
+          <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon" style="margin-left: 171px;"></a>
+          <a style="margin-left: 5px">(+84) XXX XXX XXX</a>
+          <a href=""><img src="/pics/Facebook Icon.svg" class = "img-rounded" alt="facebook-icon" style ="margin-left: 450px; margin-right: 30px; height: 35px; width: auto;"></a>
+          <a href=""><img src="/pepicase/public/pics/Group 1.svg" class = "img-rounded" alt="facebook-icon" style = "height: 35px; width: auto;"></a>
+          <a href=""><img src="/pepicase/public/pics/x.svg" class = "img-rounded" alt="facebook-icon" style ="margin-left: 30px; height: 35px; width: auto;"></a>
         </div>
-
-            <div class = "bottom-nav lexend" style = "margin-left: 10vw; padding-bottom: 20px; font-size: 20px;">
-                <a href="/pepicase/public/terms-of-use">TERMS OF USE</a>
-                <a href="/pepicase/public/policy">PRIVACY POLICY</a>
-                <a href="">COPYRIGHT INFO</a>
-                <a href="">FAQs</a>
-                <a href="">HELP</a>
-            </div>
+      </div>
         </footer>
     </body>
 </html>
