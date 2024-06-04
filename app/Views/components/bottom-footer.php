2c86c7aa4ee443215afb07a2af19d41d704da59e
tk <milkcucheo@gmail.com>
2024-06-04 12:17:32 +0700

    redesign footer

--- app/Views/components/bottom-footer.php
+++ app/Views/components/bottom-footer.php
@@ -1,23 +1,30 @@
 
-<footer class="footer" style="line-height: 60px">
-
-        <div class = "d-flex flex-row justify-content-center align-items-center">
-
-        <div class="footer-a lexend" style="font-size: 20px;line-height: 40px; width: 75vw;">
-          <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon" style="margin-left: 171px;"></a>
+<footer class="footer">
+  
+      <div class="left">
+      <div class="footer-box" style="font-size: 20px;">
+          <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon" ></a> 
+          <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon"></a>
+          <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon"></a>
+        </div>
+        <div class="footer-box">
           <a style="margin-left: 5px;">Thu Duc City, Ho Chi Minh City</a>
-          <a href="/pepicase/public/terms-of-use" style="margin-left: 350px; font-size: 20px;" id="terms-of-use">TERMS OF USE</a><br>
-          <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon" style="margin-left: 171px;"></a>
           <a style="margin-left: 5px"> pepicasesupport@gmail.com</a>
-          <a href="/pepicase/public/policy" style="margin-left: 360px; font-size: 20px;" id="terms-of-use">PRIVACY POLICY</a><br>
-          <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon" style="margin-left: 171px;"></a>
           <a style="margin-left: 5px">(+84) XXX XXX XXX</a>
-          <a href=""><img src="/pics/Facebook Icon.svg" class = "img-rounded" alt="facebook-icon" style ="margin-left: 450px; margin-right: 30px; height: 35px; width: auto;"></a>
-          <a href=""><img src="/pepicase/public/pics/Group 1.svg" class = "img-rounded" alt="facebook-icon" style = "height: 35px; width: auto;"></a>
-          <a href=""><img src="/pepicase/public/pics/x.svg" class = "img-rounded" alt="facebook-icon" style ="margin-left: 30px; height: 35px; width: auto;"></a>
         </div>
       </div>
-        </footer>
+
+      <div class="footer-box" style="margin-right: 171px;">
+          <a href="/terms-of-use" style=" font-size: 20px;" id="terms-of-use">TERMS OF USE</a>
+          <a href="/policy" style=" font-size: 20px;" id="terms-of-use">PRIVACY POLICY</a>
+          <div style="display: flex; gap: 30px;">
+          <a href=""><img src="/pepicase/public/pics/Facebook Icon.svg" class = "img-rounded" alt="facebook-icon" style ="height: 35px; width: auto;"></a>
+          <a href=""><img src="/pepicase/public/pics/Group 1.svg" class = "img-rounded" alt="facebook-icon" style = "height: 35px; width: auto;"></a>
+          <a href=""><img src="/pepicase/public/pics/x.svg" class = "img-rounded" alt="facebook-icon" style ="height: 35px; width: auto;"></a>
+          </div>
+        </div>
+      </div>
+  </footer>
     </body>
 </html>
 
