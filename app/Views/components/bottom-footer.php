2da4dbb81d3ecb27e6dbb0b6966476d8fce9161e
JohnMaxes <baodang9855.tdn@gmail.com>
2024-06-06 19:08:32 +0700

    fixed footer

--- app/Views/components/bottom-footer.php
+++ app/Views/components/bottom-footer.php
@@ -1,18 +1,25 @@
 
-<footer class="footer">
-  
-      <div class="left">
-      <div class="footer-box" style="font-size: 20px;">
-          <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon" ></a> 
-          <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon"></a>
-          <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon"></a>
+<footer class="footer" style = "height: fit-content;">
+      <div class="d-flex flex-column left">
+      <!--
+        <div class="footer-box" style="font-size: 20px;">
+          <a href=""><img src="/pepicase/public/pics/location.svg" alt="location-icon">Thu Duc City, Ho Chi Minh City</a> 
+          <a href=""><img src="/pepicase/public/pics/sms.svg" alt="sms-icon">pepicasesupport@gmail.com</a>
+          <a href=""><img src="/pepicase/public/pics/call.svg" alt="call-icon">(+84) XXX XXX XXX</a>
         </div>
         <div class="footer-box">
           <a style="margin-left: 5px;">Thu Duc City, Ho Chi Minh City</a>
           <a style="margin-left: 5px"> pepicasesupport@gmail.com</a>
           <a style="margin-left: 5px">(+84) XXX XXX XXX</a>
         </div>
+      -->
+          <a href="" style ="color:black; text-decoration:none; margin-right: 1vh;"><img src="/pepicase/public/pics/location.svg" alt="location-icon">Thu Duc City, Ho Chi Minh City</a> 
+          <a href="" style ="color:black; text-decoration:none; margin-right: 1vh;"><img src="/pepicase/public/pics/sms.svg" alt="sms-icon">pepicasesupport@gmail.com</a>
+          <a href="" style ="color:black; text-decoration:none; margin-right: 1vh;"><img src="/pepicase/public/pics/call.svg" alt="call-icon">(+84) XXX XXX XXX</a>
       </div>
+
+      
+
 
       <div class="footer-box" style="margin-right: 171px;">
           <a href="/terms-of-use" style=" font-size: 20px;" id="terms-of-use">TERMS OF USE</a>
