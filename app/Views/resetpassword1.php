94af19369c2eafdcf8707d526e8f5b0034d44ca8
JohnMaxes <baodang9855.tdn@gmail.com>
2024-06-07 22:18:48 +0700

    update UI của các reset password

--- app/Views/resetpassword1.php
+++ app/Views/resetpassword1.php
@@ -4,40 +4,41 @@
 
 
 
-<div class="lexend d-flex justify-content-center" style ="min-height: 60vh;">
-    <form id ="form" method="post" action="/pepicase/public/resetPassword">
-        <h2 class="text-center">Reset Password</h2>
-        <hr>
-        <div class="mb-3">
-            <label for="infor" class="form-label">Enter your account's email</label>
-            <input type="text" class="form-control" id="infor" name="email" placeholder="example@email.com" required>
-        </div>
-        <?php
-                    if(isset($validation))
-                    {
-                        echo "<td style ='color: red;'>";
-                        echo $validation->listErrors();
-                        echo "</td>";
-                    }
-                    if(isset($msg))
-                    {
-                        echo "<td style ='color: red;'>";
-                        echo "This account doesn't exist!";
-                        echo "</td>";
-                    }
-                    if(isset($error))
-                    {
-                        echo "<td style ='color: red;'>";
-                        echo $error;
-                        echo "</td>";
-                    }
-        ?>
-        <div class="d-grid gap-2">
-            <a class="btn btn-primary" onclick="document.getElementById('form').submit()">NEXT</a>
-            <a href="/pepicase/public/login" style ="color:black;">Go back</a>
-        </div>
-    </form>
+<div class="lexend d-flex flex-column align-items-center" style ="min-height: 60vh;">
+    <div class = "wrapper">
+        <h2 class="text-center" style="margin: 0px; margin-bottom: 20px; color: #1F3E97; font-family: 'Londrina Solid';"><b>RESET PASSWORD</b></h2>        <form id ="form" method="post" action="/pepicase/public/resetPassword">
+            <div class="mb-3">
+                <label for="infor" class="form-label"><strong>Enter your account's email:</strong></label>
+                <input type="text" class="form-control" id="infor" name="email" placeholder="example@email.com" required>
+            </div>
+            <?php
+                        if(isset($validation))
+                        {
+                            echo "<td style ='color: red;'>";
+                            echo $validation->listErrors();
+                            echo "</td>";
+                        }
+                        if(isset($msg))
+                        {
+                            echo "<td style ='color: red;'>";
+                            echo "This account doesn't exist!";
+                            echo "</td>";
+                        }
+                        if(isset($error))
+                        {
+                            echo "<td style ='color: red;'>";
+                            echo $error;
+                            echo "</td>";
+                        }
+            ?>
+            <div class="d-grid gap-2">
+                <a class="btn btn-primary" onclick="document.getElementById('form').submit()">NEXT</a>
+                <a style="color: #1F3E97; font-size:14px" href="/pepicase/public/login">Go back</a>
+            </div>
+        </form>
+    </div>
 </div>
 
 
-<?php include(APPPATH.'views/components/bottom-footer.php'); ?>
+</body>
+</html>
--- app/Views/resetpassword3.php
+++ app/Views/resetpassword3.php
@@ -1,23 +1,25 @@
 <?php include(APPPATH.'views/components/usual-links.php'); ?>
     <link rel="stylesheet" href="/pepicase/public/css/resetpassword3.css">
 <?php include(APPPATH.'views/components/top-header(no_session).php'); ?>
-        
-    <div class="d-flex justify-content-center wrapper lexend" style="height:65vh;">
-        <div class ="form-container">
+<div style = "height: 60vh;">
+    <div class="d-flex justify-content-center lexend">
+        <div class ="form-container wrapper">
             <form method="post" id="form" action="/pepicase/public/resetPassword/confirmed/<?= $encrypted_email ?>">
-                <h2 class="text-center"><strong>Reset Password</strong></h2>
+                <h2 class="text-center" style="margin: 0px; margin-bottom: 20px; color: #1F3E97; font-family: 'Londrina Solid';"><b>RESET PASSWORD</b></h2>
                 <hr>
                 <div class="mb-3">
-                    <label for="password" class="form-label"><strong>Set your password</strong></label>
+                    <label for="password" class="form-label"><strong>Set your new password:</strong></label>
                     <input type="text" class="form-control" id="password" name="password" placeholder="New password" required>
                     <input type="text" class="form-control" id="password-repeat" placeholder="Repeat new password" required>
                 </div>
                 <div class="d-grid gap-2">
                     <a class="btn btn-primary" onclick="document.getElementById('form').submit()"><strong>RESET</strong></a>
-                    <a href="/pepicase/login.php">Cancel</a>
+                    <a style="color: #1F3E97; font-size:14px" href="/pepicase/login.php">Cancel</a>
                 </div>
             </form>
         </div>
     </div>
+</div>
 
-<?php include(APPPATH.'views/components/bottom-footer.php'); ?>
+</body>
+</html>
